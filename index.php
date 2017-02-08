2c02d8deff931d90ba044ba45fa7a4e84e4bfab6
ouyangjun <ouyangjun@zhangyue.com>
2017-02-08 10:51:24 +0800

    fix:解决目录'\','/'斜线问题

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  * @version: 1.0 2017/1/13 9:47
  */
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/application/');
+define('APP_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'application' .DIRECTORY_SEPARATOR);
 // 加载框架引导文件
 require __DIR__ . '/thinkphp/start.php';
